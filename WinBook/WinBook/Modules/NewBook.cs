24b442240d808e3654971c5dd54872c3c6271726
AmerJod <Amer.j.eng@gmail.com>
2017-08-15 23:02:26 +0100

    Initial push

---
+++ WinBook/WinBook/Modules/NewBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBook.Modules
+{
+    public class NewBook
+    {
+        public int Id { get; set; }
+        public int ShortName { get; set; }
+        public int LongName { get; set; }
+
+    }
+}
