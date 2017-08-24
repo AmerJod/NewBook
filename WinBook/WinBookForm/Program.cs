7918bb70e56763ea0fcf6d44963d64c7835d56fe
PSz <cricket@cricket.hu>
2017-08-24 17:21:56 +0100

    commit my beautiful change to the master

--- WinBook/WinBookForm/Program.cs
+++ WinBook/WinBookForm/Program.cs
@@ -17,6 +17,7 @@ namespace WinBookForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Change on the master apperantly
         }
     }
 }
